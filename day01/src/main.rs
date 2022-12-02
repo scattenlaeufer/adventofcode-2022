3b0d4ef0c61a7ba34a158777dba980c975aa603f
Björn Guth <bjoern.guth@aixigo.com>
2022-12-02 08:37:38 +0100

    Solve port 2 of day 01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -29,4 +29,15 @@ fn main() {
         .map(|elve| elve.iter().sum::<i32>())
         .reduce(i32::max);
     println!("max calories for one elve: {:?}", max_calories);
+    let mut calories_by_elve = input_data
+        .iter()
+        .map(|elve| elve.iter().sum::<i32>())
+        .collect::<Vec<i32>>();
+    calories_by_elve.sort();
+    calories_by_elve.reverse();
+    let top_three_calories = &calories_by_elve[0..3];
+    println!(
+        "calories of the top three elves: {:?}",
+        top_three_calories.iter().sum::<i32>()
+    )
 }
