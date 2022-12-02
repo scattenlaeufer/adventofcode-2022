5439998e0e9dfbc73a7113ec8b0f28214457a2af
Björn Guth <bjoern.guth@aixigo.com>
2022-12-02 08:19:43 +0100

    Solve the first part of day 01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,32 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn read_file_to_vec(file_name: &str) -> Vec<Vec<i32>> {
+    let file = File::open(file_name).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut result = Vec::new();
+    let mut current_elve = Vec::new();
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if line.is_empty() {
+            result.push(current_elve);
+            current_elve = Vec::new();
+        } else {
+            current_elve.push(line.parse().unwrap())
+        }
+    }
+
+    result
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input_data = read_file_to_vec("src/input");
+    let max_calories = input_data
+        .iter()
+        .map(|elve| elve.iter().sum::<i32>())
+        .reduce(i32::max);
+    println!("max calories for one elve: {:?}", max_calories);
 }
