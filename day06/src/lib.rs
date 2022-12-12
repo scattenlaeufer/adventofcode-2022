ce1eaa04fe4d7c885f69aabdd411d02d3f69a1e4
Björn Guth <bjoern.guth@aixigo.com>
2022-12-12 18:11:29 +0100

    Solve part 2 of day 6

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -1,13 +1,13 @@
-pub fn get_start_index(input: &str) -> usize {
-    for i in 0..input.len() - 4 {
-        let sub_string = &input[i..i + 4];
+pub fn get_start_index(input: &str, n_chars: usize) -> usize {
+    for i in 0..input.len() - n_chars {
+        let sub_string = &input[i..i + n_chars];
         if sub_string
             .chars()
             .map(|c| sub_string.matches(c).count())
             .sum::<usize>()
-            == 4
+            == n_chars
         {
-            return i + 4;
+            return i + n_chars;
         }
     }
     panic!("No matching sub string found")
@@ -19,32 +19,55 @@ mod test {
 
     #[test]
     fn test_1() {
-        assert_eq!(7, get_start_index("mjqjpqmgbljsphdztnvjfqwrcgsmlb".into()))
+        assert_eq!(
+            7,
+            get_start_index("mjqjpqmgbljsphdztnvjfqwrcgsmlb".into(), 4)
+        );
+        assert_eq!(
+            19,
+            get_start_index("mjqjpqmgbljsphdztnvjfqwrcgsmlb".into(), 14)
+        );
     }
 
     #[test]
     fn test_2() {
-        assert_eq!(5, get_start_index("bvwbjplbgvbhsrlpgdmjqwftvncz".into()))
+        assert_eq!(5, get_start_index("bvwbjplbgvbhsrlpgdmjqwftvncz".into(), 4));
+        assert_eq!(
+            23,
+            get_start_index("bvwbjplbgvbhsrlpgdmjqwftvncz".into(), 14)
+        );
     }
 
     #[test]
     fn test_3() {
-        assert_eq!(6, get_start_index("nppdvjthqldpwncqszvftbrmjlhg".into()))
+        assert_eq!(6, get_start_index("nppdvjthqldpwncqszvftbrmjlhg".into(), 4));
+        assert_eq!(
+            23,
+            get_start_index("nppdvjthqldpwncqszvftbrmjlhg".into(), 14)
+        );
     }
 
     #[test]
     fn test_4() {
         assert_eq!(
             10,
-            get_start_index("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".into())
-        )
+            get_start_index("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".into(), 4)
+        );
+        assert_eq!(
+            29,
+            get_start_index("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".into(), 14)
+        );
     }
 
     #[test]
     fn test_5() {
         assert_eq!(
             11,
-            get_start_index("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".into())
-        )
+            get_start_index("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".into(), 4)
+        );
+        assert_eq!(
+            26,
+            get_start_index("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".into(), 14)
+        );
     }
 }
--- day06/src/main.rs
+++ day06/src/main.rs
@@ -3,7 +3,11 @@ use day06::get_start_index;
 fn main() {
     let input_vec = utils::read_file_to_line_vec("src/input".into());
     println!(
+        "The index of the package start is {}",
+        get_start_index(&input_vec[0], 4)
+    );
+    println!(
         "The index of the message start is {}",
-        get_start_index(&input_vec[0])
+        get_start_index(&input_vec[0], 14)
     );
 }
